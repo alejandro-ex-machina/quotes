4e37426a3ec168924775d4574822526ab240e09f
Alejandro Santos <asantos@protonmail.com>
2026-04-04 15:18:47 +0200

    Color, break de palabras

--- getquote.py
+++ getquote.py
@@ -1,30 +1,53 @@
 import json
 import random
 from pathlib import Path
-from typing import Final
+import shutil
+import textwrap
 
-QUOTES_FILE  = Path(__file__).with_name("quotes.json")
+GREEN = "\033[32m"
+GRAY = "\033[90m"
+RESET = "\033[0m"
 
-with QUOTES_FILE.open ("r", encoding="utf-8") as f:
+QUOTES_FILE  = "quotes.json"
+
+
+with Path(__file__).with_name(QUOTES_FILE).open ("r", encoding="utf-8" ) as f:
     quotes = json.load (f)
 
-
 if not quotes:
-    raise SystemExit ( QUOTES_FILE._str() + " esta vacio.")
+    raise SystemExit ( QUOTES_FILE + " esta vacio." )
 
+def get_terminal_width ( margin=4, min_width=40 ) :
+    width = shutil.get_terminal_size(fallback=(80, 20)).columns - margin
+    return max(min_width, width)
+
+def wrap_text(text, width):
+    return textwrap.fill(
+        text,
+        width=width,
+        break_long_words=False,
+        break_on_hyphens=False
+    )
 def quote() -> None:
     quote = random.choice (quotes)
     
+    # Obtener valores y formatear
+
     text   = quote.get ("quote", "").strip()
     author = quote.get ("author", "").strip()
-    ref    = " (" + quote.get ("ref", "").strip() + ")" if quote.get ("ref", "").strip() else ""
+    ref    = "- " + quote.get ("ref", "").strip() if quote.get ("ref", "").strip() else ""
 
-    author = "\033[1m"+ author + "\033[0m" + ref    
+    #  Color
 
-    print (f'\n«{text}».\n')
-    print (f'{author}\n')
+    author = f"{GREEN}{author}{RESET} {GRAY}{ref}{RESET}"
+
+    width = get_terminal_width()
+
+    # Maquetar y salida
+
+    print(wrap_text( f'«{text}».' , width))
+    print (f'\n{author}\n')
     print (len(quotes), "quotes\n")
-
 
 if __name__ == "__main__":
     quote()
